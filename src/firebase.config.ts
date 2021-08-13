7ef312c7c96d262cb9d3e1aa2680a923a0454d18
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-08-13 20:47:33 +0900

    add firebase config

---
+++ src/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDWRHMyxX5YbRY_qypQYkGEOo3zxzjZrU",
+  authDomain: "antonve-test-firebase.firebaseapp.com",
+  projectId: "antonve-test-firebase",
+  storageBucket: "antonve-test-firebase.appspot.com",
+  messagingSenderId: "246939419667",
+  appId: "1:246939419667:web:70ff72ca9250a2a201abe6",
+  measurementId: "G-CHN1WB56BJ",
+};
